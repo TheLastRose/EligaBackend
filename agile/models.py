4143af81af802c47470d46b3012d6586105eac02
TheLastRose <s25165791@gmail.com>
2020-02-19 17:11:31 -0800

    Model modify

--- agile/models.py
+++ agile/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 class product_backlog(models.Model):
     #backlogID   = models.AutoField(primary_key=True)
+    sprintID          = models.PositiveIntegerField(default= 0)
     content           = models.TextField()
     priority          = models.CharField(max_length=5)
     hours             = models.PositiveIntegerField()
     dateCreated       = models.DateTimeField(auto_now=True)
 
 class sprint_backlog(models.Model):
-    productbacklogID    = models.PositiveIntegerField()
+    sprintID            = models.PositiveIntegerField()
     content             = models.TextField()
-    status              = models.PositiveIntegerField()
     dateCreated         = models.DateTimeField(auto_now=True)
 
 class sprint(models.Model):
-    sprintbacklogID     = models.PositiveIntegerField()
-    dateCreated         = models.DateTimeField(auto_now=True)
+    startDate         = models.DateField()
+    dueDate           = models.DateField()
 
 class task_model(models.Model):
     sprintbacklogID     = models.PositiveIntegerField()
     content             = models.TextField()
-    responsible         = models.PositiveIntegerField()
+    assignTo            = models.PositiveIntegerField()
     status              = models.PositiveIntegerField()
     dateCreated         = models.DateTimeField(auto_now=True)
 
