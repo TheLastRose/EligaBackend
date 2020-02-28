5c95b698d346e9285b5e8f848d387a5e3fe8b26e
TheLastRose <s25165791@gmail.com>
2020-02-27 16:46:13 -0800

    ERD V8 changes

--- agile/models.py
+++ agile/models.py
@@ -2,31 +2,40 @@ from django.db import models
 
 # Create your models here.
 class product_backlog(models.Model):
-    sprintID          = models.PositiveIntegerField(default= 0)
+    sprintbacklogID   = models.PositiveIntegerField(default= 0)
     content           = models.TextField()
     priority          = models.CharField(max_length=5)
     hours             = models.PositiveIntegerField()
     dateCreated       = models.DateTimeField(auto_now=True)
 
-class sprint(models.Model):
+class sprint_backlog(models.Model):
     startDate         = models.DateField()
     dueDate           = models.DateField()
+    content           = models.TextField(default= ' ')
     name              = models.TextField(default= 'Sprint Name')
 
-class sprint_backlog(models.Model):
-    sprintID            = models.PositiveIntegerField()
-    content             = models.TextField()
-    dateCreated         = models.DateTimeField(auto_now=True)
+# class sprint_backlog(models.Model):
+#     sprintID            = models.PositiveIntegerField()
+#     content             = models.TextField()
+#     dateCreated         = models.DateTimeField(auto_now=True)
 
 class task_model(models.Model):
-    sprintbacklogID     = models.PositiveIntegerField()
+    sprintbacklogID     = models.PositiveIntegerField(default= 0)
+    productbacklogID    = models.PositiveIntegerField(default= 0)
     content             = models.TextField()
     assignTo            = models.PositiveIntegerField(default= 0)
-    status              = models.PositiveIntegerField(default= 0)
+    dateCompleted       = models.DateField(auto_now=True)
+    hours               = models.PositiveIntegerField(default= 0)
+    status              = models.CharField(max_length=10)
     dateCreated         = models.DateTimeField(auto_now=True)
+
+# class task_status(models.Model):
+#     date        = models.DateField()
+#     totalTasks  = models.PositiveIntegerField()
+#     remainTasks = models.PositiveIntegerField()
 
 class user_model(models.Model):
     name        = models.CharField(max_length=15,default='Name of User')
-    username    = models.CharField(max_length=25)
-    password    = models.CharField(max_length=25)
+    role        = models.CharField(max_length=15,default='User Role')
     dateCreated = models.DateTimeField(auto_now=True)
+    #name, role, avatar
