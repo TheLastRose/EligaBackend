47252c1cd1e06cc017eab600056aa9846eb6205b
TheLastRose <s25165791@gmail.com>
2020-02-27 16:55:58 -0800

    New filters for all models

--- agile/viewsets.py
+++ agile/viewsets.py
@@ -6,7 +6,7 @@ from .serializers import *
 class ProductbacklogViewSet(viewsets.ModelViewSet):
     queryset = product_backlog.objects.all()
     serializer_class =  ProductbacklogSerializer
-    filter_fields = ('sprintID',)
+    filter_fields = ('sprintbacklogID',)
 
 # class SprintViewSet(viewsets.ModelViewSet):
 #     queryset = sprint.objects.all()
@@ -15,12 +15,12 @@ class ProductbacklogViewSet(viewsets.ModelViewSet):
 class SprintbacklogViewSet(viewsets.ModelViewSet):
     queryset = sprint_backlog.objects.all()
     serializer_class =  SprintbacklogSerializer
-    filter_fields = ('sprintID',)
+    # filter_fields = (,)
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = task_model.objects.all()
     serializer_class =  TaskSerializer
-    filter_fields = ('sprintbacklogID',)
+    filter_fields = ('sprintbacklogID','productbacklogID','assignTo','dateCompleted','status',)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = user_model.objects.all()
