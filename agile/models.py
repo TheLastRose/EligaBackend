c97bcdf559d8aab3ff0c9e2d3c24ab2a5e0cd816
TheLastRose <s25165791@gmail.com>
2020-02-28 16:26:46 -0800

    Squence reset test, datecompeleted fixed

--- agile/models.py
+++ agile/models.py
@@ -12,28 +12,17 @@ class product_backlog(models.Model):
 class sprint_backlog(models.Model):
     startDate         = models.DateField(default=datetime.date.today)
     dueDate           = models.DateField(default=datetime.date.today)
-    # content           = models.TextField(default= ' ')
     name              = models.TextField(default= 'Sprint Name')
-
-# class sprint_backlog(models.Model):
-#     sprintID            = models.PositiveIntegerField()
-#     content             = models.TextField()
-#     dateCreated         = models.DateTimeField(auto_now=True)
 
 class task_model(models.Model):
     sprintbacklogID     = models.PositiveIntegerField(default= 0)
     productbacklogID    = models.PositiveIntegerField(default= 0)
     content             = models.TextField()
     assignTo            = models.PositiveIntegerField(default= 0)
-    dateCompleted       = models.DateField()
+    dateCompleted       = models.DateField(blank=True) #Remember set =null in DB
     hours               = models.PositiveIntegerField(default= 0)
     status              = models.CharField(max_length=10, default= 'Incomplete')
     dateCreated         = models.DateTimeField(auto_now=True)
-
-# class task_status(models.Model):
-#     date        = models.DateField()
-#     totalTasks  = models.PositiveIntegerField()
-#     remainTasks = models.PositiveIntegerField()
 
 class user_model(models.Model):
     name        = models.CharField(max_length=15,default='Name of User')
