911a1d4b9737cba01c8d06cfd89ebdf7219b4e2f
TheLastRose <s25165791@gmail.com>
2020-03-03 16:16:51 -0800

    allow_all to false

--- eliga_api/settings.py
+++ eliga_api/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True # Edit this after development
+CORS_ORIGIN_ALLOW_ALL = False # Edit this after development
 
 CORS_ALLOW_METHODS = [
     'DELETE',
